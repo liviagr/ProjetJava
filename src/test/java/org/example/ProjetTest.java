36c7a9c69454b116e68601bca97afea245bd4134
liviagr <113026321+liviagr@users.noreply.github.com>
2024-01-19 14:01:40 +0100

    test intégration projet

--- src/test/java/org/example/ProjetTest.java
+++ src/test/java/org/example/ProjetTest.java
@@ -5,7 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class ProjetTest {
 
+//    Test intégration : ajout des tâches
+//    Les tâches sont identiques quelles soient ajoutées par projet ou par le gestionnaire de tâche
+    @Test
+    public void ajouterTache(){
 
+//        créer la partie "expected"
+        Projet projet = new Projet("projet1");
+        projet.ajouterTache(new Tache("titre1", "description1", 2));
+
+        Tache tacheExpected = projet.getListeTache().get(0);
+
+//        créer la partie "test"
+        Projet projet2 = new Projet("projet2");
+        GestionTache gestionTache = new GestionTache(projet2);
+        gestionTache.ajouterTache("titre1", "description1", 2);
+        Tache tacheTest = projet2.getListeTache().get(0);
+
+        Assertions.assertTrue(tacheExpected.getTitre().equals(tacheTest.getTitre()) && tacheExpected.getDescription().equals(tacheTest.getDescription()) &&
+                tacheExpected.getEtat()==tacheTest.getEtat());
+
+    }
 
 //    cas commun : liste pleine et durées positives
     @Test
